9c907670fcc81829021c8a3b9e84cc590af0f387
gouree <goure.deshpande@gmail.com>
2017-03-21 18:59:36 +0530

    Add files via upload

---
+++ AddTwoNumbers.java
@@ -0,0 +1,16 @@
+
+public class AddTwoNumbers {
+
+	public static void main (String args[])
+		  {
+			  System.out.println("Addition of two integers without using + operator");
+			  int a=40,b=10,c;
+			  c= a-(-b);
+			  System.out.println("a="+a);
+			  System.out.println("b="+b);
+			  System.out.println(a+"+"+b+"="+c);
+		  }
+	}
+
+
+
